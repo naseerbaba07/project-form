e831dd94a216c70a85988b779647764d98f54a75
Naseer baba <23eg105v15@anurag.edu.in>
2026-03-15 18:42:22 +0530

    Added health api

--- server/src/main/java/com/example/myapp/controller/HomeController.java
+++ server/src/main/java/com/example/myapp/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.example.myapp.controller;
 
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class HomeController {
     public Map<String, String> home() {
         return Map.of("message", "Project loaded successfully ✅");
     }
+
+    @GetMapping("/health")
+    public ResponseEntity<?> health() {
+    return ResponseEntity.ok("Server is running ✅  ");
+}
 
 
 }
